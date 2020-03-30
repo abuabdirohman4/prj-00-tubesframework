8758a80beb624366dadc5b5059d4820aa2fcd765
Bagus Seno Pamungkas <bagusseno354@gmail.com>
2020-03-30 19:55:13 +0700

    Commit before pulling

--- application/controllers/report.php
+++ application/controllers/report.php
@@ -1,10 +1,21 @@
 <?php
 
-class Coa extends CI_controller
+class Report extends CI_controller
 {
 
     public function __constructor() {
 
         $this->load->model('pembelian_model');
+        $this->load->model('penjualan_model');
+
+    }
+
+    public function index() {
+
+        $total_barang_terjual = $this->pembelian_model->;
+        $total_stok_tersisa;
+        $total_income;
+        $total_profit;
+
     }
 }
